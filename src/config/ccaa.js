ce6ea921da99f2e171cf5e65644172a90f50c63c
Luis Pedraz <LPedraz1981@gmail.com>
2020-03-28 23:38:19 +0100

    add ccaa location data

---
+++ src/config/ccaa.js
@@ -0,0 +1,43 @@
+export const CCAA_DATA = [{
+        name: 'Andalucía',
+        coords: {
+            lon: 37.667033,
+            lat: -4.694910
+        }
+    },
+    {
+        name: 'Aragón',
+        coords: {
+            lon: 41.766628,
+            lat: -0.845947
+        }
+    },
+    {
+        name: 'Asturias',
+        coords: {
+            lon: 43.361011,
+            lat: -5.852427
+        }
+    },
+    {
+        name: 'Baleares',
+        coords: {
+            lon: 39.591688,
+            lat: 2.964278
+        }
+    },
+    {
+        name: 'Canarias',
+        coords: {
+            lon: 28.435723,
+            lat: -16.646957
+        }
+    },
+    {
+        name: 'Cantabria',
+        coords: {
+            lon: 43.224242,
+            lat: -3.898213
+        }
+    }
+];
