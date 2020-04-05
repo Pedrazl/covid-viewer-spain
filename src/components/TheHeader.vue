cbb6458899949949acfc40f7de9e9421538449ea
Luis Pedraz <LPedraz1981@gmail.com>
2020-04-06 01:35:40 +0200

    Add header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <h1 class="title is-2">
+      Crísis COVID-19 en España
+    </h1>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+.header {
+  padding: 1rem;
+  background-color: #333;
+  border-bottom: 1px solid grey;
+}
+.header h1 {
+  color: #ffffff;
+
+}
+</style>
