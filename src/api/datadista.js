a7f1b245492e7a11b266dc1c38c826dc4de1c755
Luis Pedraz <LPedraz1981@gmail.com>
2020-04-11 01:35:21 +0200

    Add method to get national data

--- src/api/datadista.js
+++ src/api/datadista.js
@@ -1,18 +1,28 @@
 import axios from "axios";
-
+import Papa from "papaparse";
 const _basicHeaders = {
   "Content-Type": "text/csv",
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS"
+  "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
 };
 
-const getCases = async () => {
+const getNationalData = async () => {
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  const url = `https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/nacional_covid19.csv`;
+  const res = await axios.get(proxyurl + url, {
+    headers: _basicHeaders,
+  });
+  return Papa.parse(res.data, { header: true });
+};
+
+const getCasesByRegion = async () => {
   const proxyurl = "https://cors-anywhere.herokuapp.com/";
   const url = `https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_casos.csv`;
   const res = await axios.get(proxyurl + url, {
-    headers: _basicHeaders
+    headers: _basicHeaders,
   });
-  return res.data;
+
+  return Papa.parse(res.data, { header: true });
 };
 
-export { getCases };
+export { getCasesByRegion, getNationalData };
