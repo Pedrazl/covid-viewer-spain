518c1c02b2cd632581f8e74f0a64da095de21ca5
Luis Pedraz <LPedraz1981@gmail.com>
2020-04-25 22:30:42 +0200

    Add vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,39 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import * as types from "./types";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    regionalData: {
+      cases: [],
+      deaths: [],
+      recovered: []
+    },
+    nationalData: {
+      today: {},
+      yesterday: {}
+    }
+  },
+  mutations: {
+    [types.SET_NATIONAL_DATA](state, data) {
+      state.nationalData.today = data.today;
+      state.nationalData.yesterday = data.yesterday;
+    },
+    [types.SET_REGIONAL_DATA](state, data) {
+      state.regionalData.cases = data.cases;
+      state.regionalData.recovered = data.recovered;
+      state.regionalData.deaths = data.deaths;
+    }
+  },
+  actions: {
+    setNationalData(context, data) {
+      context.commit(types.SET_NATIONAL_DATA, data);
+    },
+    setRegionalData(context, data) {
+      context.commit(types.SET_REGIONAL_DATA, data);
+    }
+  },
   modules: {}
 });
