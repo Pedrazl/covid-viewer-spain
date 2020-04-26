872172b6b4b60b00c04663b649fa7b99dc22396e
Luis Pedraz <LPedraz1981@gmail.com>
2020-04-27 00:35:27 +0200

    Add datadista reference on map

--- src/components/TheMap.vue
+++ src/components/TheMap.vue
@@ -56,7 +56,7 @@ export default {
         "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
         {
           attribution:
-            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>| Data from <a href="https://datadista.com/">Datadista</a>',
           subdomains: "abcd",
           maxZoom: 19
         }
