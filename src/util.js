4eb78e146148a9cd315f7369acd7d0cd868605ac
Luis Pedraz <LPedraz1981@gmail.com>
2020-05-04 22:49:30 +0200

    Add daily stats method

--- src/util.js
+++ src/util.js
@@ -1,3 +1,10 @@
 export const calculateTrend = (present, past) => {
   return (((present - past) * 100) / present).toFixed(1);
 };
+
+export const getDailyStats = (today, yesterday) => {
+  return {
+    diff: today - yesterday,
+    trend: calculateTrend(today, yesterday)
+  };
+};
