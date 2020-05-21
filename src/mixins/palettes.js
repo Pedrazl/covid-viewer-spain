7998da41f18f01802c17a495fa55e340a3788c69
Luis Pedraz <LPedraz1981@gmail.com>
2020-05-22 01:58:58 +0200

    New color ramps

--- src/mixins/palettes.js
+++ src/mixins/palettes.js
@@ -11,55 +11,55 @@ export const palettesMixin = {
       }
     },
     getPurple(d) {
-      return d > 10
+      return d > 8
         ? "#4d004b"
-        : d > 5.5
+        : d > 4
         ? "#810f7c"
-        : d > 4.5
+        : d > 2
         ? "#88419d"
-        : d > 3.5
-        ? "#8c6bb1"
-        : d > 2.5
-        ? "#8c96c6"
-        : d > 1.5
-        ? "#9ebcda"
         : d > 1
+        ? "#8c6bb1"
+        : d > 0.8
+        ? "#8c96c6"
+        : d > 0.4
+        ? "#9ebcda"
+        : d > 0.2
         ? "#bfd3e6"
         : "#e0ecf4";
     },
 
     getRed(d) {
-      return d > 10
+      return d > 8
         ? "#67000d"
-        : d > 5.5
+        : d > 4
         ? "#a50f15"
-        : d > 4.5
+        : d > 2
         ? "#cb181d"
-        : d > 3.5
-        ? "#ef3b2c"
-        : d > 2.5
-        ? "#fb6a4a"
-        : d > 1.5
-        ? "#fc9272"
         : d > 1
+        ? "#ef3b2c"
+        : d > 0.8
+        ? "#fb6a4a"
+        : d > 0.4
+        ? "#fc9272"
+        : d > 0.2
         ? "#fcbba1"
         : "#fee0d2";
     },
 
     getGreen(d) {
-      return d > 10
+      return d > 8
         ? "#005a32"
-        : d > 5.5
+        : d > 4
         ? "#238b45"
-        : d > 4.5
+        : d > 2
         ? "#41ab5d"
-        : d > 3.5
-        ? "#74c476"
-        : d > 2.5
-        ? "#a1d99b"
-        : d > 1.5
-        ? "#c7e9c0"
         : d > 1
+        ? "#74c476"
+        : d > 0.8
+        ? "#a1d99b"
+        : d > 0.4
+        ? "#c7e9c0"
+        : d > 0.2
         ? "#e5f5e0"
         : "#f7fcf5";
     }
