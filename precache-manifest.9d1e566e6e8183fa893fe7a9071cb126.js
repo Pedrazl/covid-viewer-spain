b254719652d3cc2b25f1e3aeb23639804d30f8b8
Pedrazl <LPedraz1981@gmail.com>
2020-10-27 21:23:18 +0000

    gh-pages

---
+++ precache-manifest.9d1e566e6e8183fa893fe7a9071cb126.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e931a434dd2ac36d27e9",
+    "url": "/covid-viewer-spain/css/app.406e2586.css"
+  },
+  {
+    "revision": "2a26c8fe52fcb73eaa22",
+    "url": "/covid-viewer-spain/css/chunk-vendors.33eb119a.css"
+  },
+  {
+    "revision": "d2863522b7c27ddeabf58fa927a2247e",
+    "url": "/covid-viewer-spain/img/Covid_Viewer_Screenshot.PNG"
+  },
+  {
+    "revision": "2f34bd27dc4068b8ec155e45b2a29909",
+    "url": "/covid-viewer-spain/img/Covid_Viewer_Screenshot_28_04_2020.PNG"
+  },
+  {
+    "revision": "bfc773d722944008c1b1ec5ca58d9a86",
+    "url": "/covid-viewer-spain/img/Demo1.gif"
+  },
+  {
+    "revision": "892584a83f30cb5c5e3606e531212fac",
+    "url": "/covid-viewer-spain/index.html"
+  },
+  {
+    "revision": "e931a434dd2ac36d27e9",
+    "url": "/covid-viewer-spain/js/app.bcbe51ab.js"
+  },
+  {
+    "revision": "2a26c8fe52fcb73eaa22",
+    "url": "/covid-viewer-spain/js/chunk-vendors.e6489b44.js"
+  },
+  {
+    "revision": "2f99e28a143796df53936f2e01e3092f",
+    "url": "/covid-viewer-spain/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/covid-viewer-spain/robots.txt"
+  }
+]);
